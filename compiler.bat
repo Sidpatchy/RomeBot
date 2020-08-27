2e12ecba4b57d11ec55204e4644256906fb4fbbd
Sidpatchy <rainverm38@gmail.com>
2020-08-26 21:50:38 -0500

    Update compile instructions and batch file for v2
    Title thing says it all. Also wrote a new wiki page for using RomeBot v2.

--- compiler.bat
+++ compiler.bat
@@ -1,16 +1,13 @@
 @echo off
 
-echo This simple script allows you to compile RomeBot by Rainverm38
+echo This simple script allows you to compile RomeBot by Sidpatchy
+echo For a more detailed guide, please check the wiki: https://github.com/Sidpatchy/RomeBot/wiki/How-to-use-RomeBot.py-(version-2)
 
 python -m pip install –upgrade pip    # Remove this line if you do not want PIP to be updated
 
 pip install pyinstaller
 pip install asyncio
 pip install discord.py
-
-set /P token=Insert Your Bot Token: 
-
-echo bot.run('%token%')       # User defined bot token, get one here: https://discordapp.com/developers/applications/ >> RomeBot.py
 
 pyinstaller -F RomeBot.py
 
